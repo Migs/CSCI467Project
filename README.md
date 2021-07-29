a0c6cec36cf9b69cfb1ace943c64553027c5934d
Miguel Williams <miguel_williams7@yahoo.com>
2021-07-29 18:40:12 -0500

    Initial starter code for group project

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-Initial README
-To test file stuff and see if it actually works
-
+# express-starter-product
---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+var port = process.env.PORT || 3000;
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('index');
+})
+
+const parts = require('./controllers/parts');
+app.get('/getParts', (req, res) => {
+  parts.getAll((list) => {
+    res.render('parts.ejs', { all: list });
+  });
+})
+
+const credit = require('./controllers/credit');
+app.get('/processCC', (req, res) => {
+  credit.processSample((result) => {
+    res.render('credit.ejs', { data: result });
+  });
+})
+
+app.listen(port, () => {
+  console.log(`Express server listening at http://localhost:${port}`)
+})
