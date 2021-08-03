d3b066f0481e7629f8a0268bb6ba4be87ad31a30
Miguel Williams <miguel_williams7@yahoo.com>
2021-08-03 00:52:18 -0500

    finished associate page and quote insertion

--- group-project/group_project/src/views/setquote.jsx
+++ group-project/group_project/src/views/setquote.jsx
@@ -1,16 +1,33 @@
-import {React, useState} from 'react';
+import {React, useState, useEffect} from 'react';
 import {useLocation } from 'react-router-dom';
-import {TextField} from '@material-ui/core';
+import {Button, Checkbox, TextField} from '@material-ui/core';
+import axios from 'axios';
 
 function SetQuote(){
     const location = useLocation();
     const [inputList, setInputList] = useState([{ lineItem: '', price: 0, description: ''}]);
     const [notesList, setNotesList] = useState([{notes: ''}]);
+    const [associateid, setAssociateId] = useState(null);
+
+    const [quote, setQuotes] = useState({ email: '', discount: 0})
+
+    var [percentdiscount, setPercentDiscount] = useState(false);
+
     var [totalprice, setTotalPrice] = useState(0);
+    var [reducedprice, setReducedPrice] = useState(0);
 
     const sumInputList = () => {
         const tprice = inputList.reduce((a, { price }) => parseInt(a) + parseInt(price), 0);
+        var rprice = 0;
+
+        if(percentdiscount){
+            rprice = tprice - tprice * (quote.discount/100);
+        }
+        else{
+            rprice = tprice - quote.discount;
+        }
         setTotalPrice(tprice);
+        setReducedPrice(rprice);
     };
 
     const handleInputChange = (e, index) => {
@@ -49,17 +66,44 @@ function SetQuote(){
 
     const handleAddNotesClick = () => {
         setNotesList([...notesList, { notes: ''}]);
-        
     };
+
+    const handleDiscountChange = (e) => {
+        setQuotes({
+            email: quote.email,
+            discount: parseInt(e.target.value)
+        });
+    }
+
+    const handleEmailChange = (e) => {
+        
+        setQuotes({
+            email: e.target.value,
+            discount: quote.discount
+        });
+    }
+
+    const submitQuote = () => {
+        console.log(location.state.data);
+        axios.post('/quotes/null/' + location.state.data.id +'/'+associateid+'/'+totalprice+'/0/0/'+percentdiscount+'/'+quote.discount+'/'+quote.email).then((res) => {
+                console.log(res.data);
+            });
+    }
 
     return(
         <>
             <form>
                 <TextField disabled id="customerid" label="Name" defaultValue={location.state.data.name} /><br />
-                <TextField required id="associateid" label="Associate Name" /><br />
-                <TextField required id="discountpercent" label="Discount %" type="number" /><br />
-                <TextField required id="discountflat" label="Discount Flat" type="number" /><br />
-                <TextField required id="email" label="Email" /><br />
+                <TextField required id="associate" label="Associate Name" />
+                <TextField required id="associateid" label="Associate ID" onChange={e => {setAssociateId(e.target.value);}} /><br />
+
+                <Checkbox checked={percentdiscount} name="discounttype" label="Percent Discount?" onChange={e => {
+                    setPercentDiscount(e.target.checked)
+                    sumInputList()
+                }}></Checkbox>Percent Discount?<br />
+
+                <TextField required id="discountflat" label="Discount" type="number" onChange={e => handleDiscountChange(e)} /><br />
+                <TextField required id="email" label="Email" onChange={e => handleEmailChange(e)} /><br />
                 {notesList.map((x, i) => {
                     return (
                         <>
@@ -73,7 +117,8 @@ function SetQuote(){
                 })
                     
                 }
-                <TextField disabled id="price" label="Price" type="number" defaultValue={totalprice} value={totalprice}/><br />
+                <TextField disabled id="price" label="Price" type="number" defaultValue={totalprice} value={totalprice}/>
+                <TextField disabled id="reducedprice" label="Reduced Price" type="number" defaultValue={reducedprice} value={reducedprice}/><br />
                 {inputList.map((x, i) => {
                     return (
                         <>
@@ -87,6 +132,7 @@ function SetQuote(){
                         </>
                     );
                 })}
+                <Button variant="outlined" onClick={submitQuote}>Submit</Button>
             </form>
         </>
     );
